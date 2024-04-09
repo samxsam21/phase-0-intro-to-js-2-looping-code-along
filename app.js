e9b2b2a696dfb33b91444fd1d40a05976b3a18f1
samxsam21 <moe.amayreh@gmail.com>
2024-04-09 23:03:17 +0300

    mywork

---
+++ app.js
@@ -0,0 +1,11 @@
+function writeCards(names, event) {
+    const messages = [];
+    for (let i = 0; i < names.length; i++) {
+      messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return messages;
+  }
+  
+  const messages = writeCards(["Charlie", "Samip", "Ali"], "birthday");
+  console.log(messages);
+  
---
+++ app2.js
@@ -0,0 +1,9 @@
+function countDown(num) {
+    let i = num;
+    while (i >= 0) {
+      console.log(i);
+      i--;
+    }
+  }
+  countDown(10);
+  
